64a4eb98cd5222bf2f6fbe37f33d07d610f6b037
brunibunny <33870163+brunibunny@users.noreply.github.com>
2018-07-26 14:12:31 -0300

    Minor minor fix

--- VNXTLP/Properties/AssemblyInfo.cs
+++ VNXTLP/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("VNX+ Translation Platform")]
-[assembly: AssemblyDescription("Translation Platform to VNX+ Projects")]
+[assembly: AssemblyDescription("Translation Platform to use on VNX+ Projects")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("VNX+")]
 [assembly: AssemblyProduct("VNXTLP")]
